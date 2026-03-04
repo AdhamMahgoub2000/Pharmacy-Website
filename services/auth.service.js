1db292ab3c9212658def08e182602bb8ad911ec8
Adham Mahgoub <hd.adham50@gmail.com>
2026-03-04 19:08:57 +0200

    Add Auth Service

--- services/auth.service.js
+++ services/auth.service.js
@@ -1,7 +1,33 @@
 angular.module('pharmacyApp')
-.service('AuthService', function() {
-    this.getCurrentUser = function() {
-        
+.service('AuthService', ['SupabaseService', function(SupabaseService) {
+
+    const client = SupabaseService.client;
+
+    this.login = async function(email, password) {
+        return await client.auth.signInWithPassword({
+            email: email,
+            password: password
+        });
     };
-    
-});
+    this.getUser = async function () {
+        const { data } = await client.auth.getUser();
+        return data.user;
+        };
+
+    this.getUserData = async function (userId) {
+      const { data } = await client
+        .from("users")
+        .select("*")
+        .eq("id", userId)
+        .single();
+      return data;
+    };
+
+    this.logout = async function () {
+      await client.auth.signOut();
+    };
+    this.verifySession = async function () {
+      const { data, error } = await client.auth.getSession();
+      return { session: data?.session, error };
+    };
+}]);
